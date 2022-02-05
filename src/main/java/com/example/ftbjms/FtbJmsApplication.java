f2a4508020a30899007be6b1a2a9e574120bfdbd
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-02-05 12:18:42 +0200

    Initial commit

---
+++ src/main/java/com/example/ftbjms/FtbJmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ftbjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtbJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtbJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ftbjms/FtbJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ftbjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtbJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
